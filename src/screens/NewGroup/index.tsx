5f7652d980c3b39531fc568e9ed29d187819acb9
Clelton Mielke <60109916+cleltonjm@users.noreply.github.com>
2022-10-01 23:55:36 -0300

    Add files via upload

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,15 +1,16 @@
 import { useState } from 'react';
+import { Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
+import { AppError } from '@utils/AppError';
 import { groupCreate } from '@storage/group/groupCreate';
+
+import { Container, Content, Icon } from './styles';
 
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Button } from '@components/Button';
 import { Input } from '@components/Input';
-
-import { useNavigation } from '@react-navigation/native';
-
-import { Container, Content, Icon } from './styles';
 
 export function NewGroup() {
     const [group, setGroup] = useState('');
@@ -18,10 +19,19 @@ export function NewGroup() {
     
     async function handleNew() {
         try {
+            if(group.trim().length === 0) {
+                return Alert.alert('Novo grupo', 'Informe o nome da turma.');
+            }
+
             await groupCreate(group);
             navigation.navigate('players', { group });
         } catch (error) {
-          console.log(error);
+            if (error instanceof AppError) {
+                Alert.alert('Novo grupo', error.message);
+            } else {
+                Alert.alert('Novo grupo', 'Não foi possível criar um novo grupo.');
+                console.log(error);
+            }
         }
     }
 
